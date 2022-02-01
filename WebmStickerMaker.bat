3cef068e337b56018fb238b822469670d28355d4
Mehran <47609307+meh7an@users.noreply.github.com>
2022-02-01 04:20:23 +0300

    Update WebmStickerMaker.bat

--- WebmStickerMaker.bat
+++ WebmStickerMaker.bat
@@ -4,6 +4,6 @@
 @PAUSE
 @EXIT /B
 )
-@for %%f in (*.mp4 *.mov *.flv *.mkv) do ffmpeg.exe -i %%f -c:v libvpx-vp9 -vf scale=512:512 -an %%f_Sticker.webm -hide_banner -loglevel error
+@for %%f in (*.mp4 *.mov *.avi *.flv *.mkv) do ffmpeg.exe -i %%f -c:v libvpx-vp9 -vf scale=512:512 -an %%f_Sticker.webm -hide_banner -loglevel error
 @ECHO All videos are converted to stickers.
 @PAUSE
